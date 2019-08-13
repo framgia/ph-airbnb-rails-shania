b68c25ea5f67b0b99f140a0c2fe804090fc0dd55
Jubelag <35232348+shaniajubelag@users.noreply.github.com>
2019-08-13 14:22:05 +0800

    Task Toastr setup (#5)
    * Toastr setup
    
    * [TOASTR] Removed unnecessary files

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bcrypt', '~> 3.1.13'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise', '~> 4.2'
 gem 'jquery-rails'
+gem 'toastr-rails'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  # Toastr
+  def custom_bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'error'   if type == 'alert'
+      puts type
+
+      text = "<script>toastr.#{type}('#{message}');</script>"
+      flash_messages << text.html_safe if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
