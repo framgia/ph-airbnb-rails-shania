9f723a6a4de3d532314cf98fa0822e3ff75b1066
Jubelag <35232348+shaniajubelag@users.noreply.github.com>
2019-08-14 16:16:14 +0800

    Task User profile page (#6)
    * User profile page
    
    * [PROFILE] Added edit profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "PokeHost"
+    base_title="PokeHost"
     if page_title.empty?
       base_title
     else
@@ -17,7 +17,7 @@ module ApplicationHelper
       type = 'error'   if type == 'alert'
       puts type
 
-      text = "<script>toastr.#{type}('#{message}');</script>"
+      text="<script>toastr.#{type}('#{message}');</script>"
       flash_messages << text.html_safe if message
     end
     flash_messages.join("\n").html_safe
--- db/migrate/20190808112546_add_name_to_users.rb
+++ db/migrate/20190808112546_add_name_to_users.rb
@@ -1,5 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
+    add_column :users, :phonenumber, :string
+    add_column :users, :description, :string
   end
 end
