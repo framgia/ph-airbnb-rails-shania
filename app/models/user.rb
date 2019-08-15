4324761c19fa556994ed20c01ba9523b63f3250c
shania98 <shania.jubelag@sun-asterisk.com>
2019-08-15 17:24:05 +0800

    [BUG - FB AUTH] Added condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
-  
-  # Omniauth with Facebook
+         
+  # Signup with Facebook
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.provider = auth.provider
-      user.name = auth.info.name
-      user.uid = auth.uid
-      user.password = Devise.friendly_token[0,20]
-      user.image = auth.info.image
-      user.skip_confirmation! #skips email confirmation
+      if user.email?
+        return user
+      else
+        user.email = auth.info.email
+        user.provider = auth.provider
+        user.name = auth.info.name
+        user.uid = auth.uid
+        user.password = Devise.friendly_token[0,20]
+        user.image = auth.info.image
+        user.skip_confirmation! #skips email confirmation
+      end
     end
   end
+
 end
