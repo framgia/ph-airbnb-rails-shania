93438d5d71fc78a8b711c55af5e0970d864f636f
shania98 <shania.jubelag@sun-asterisk.com>
2019-08-15 18:57:56 +0800

    [BUG - FB AUTH] Redid condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,20 @@ class User < ApplicationRecord
          
   # Signup with Facebook
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      if user.email?
-        return user
-      else
-        user.email = auth.info.email
-        user.provider = auth.provider
-        user.name = auth.info.name
-        user.uid = auth.uid
-        user.password = Devise.friendly_token[0,20]
-        user.image = auth.info.image
-        user.skip_confirmation! #skips email confirmation
-      end
-    end
+    user = User.find_by(email: auth.info.email)
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.uid = auth.uid
+      user.password = Devise.friendly_token[0,20]
+      user.image = auth.info.image
+      user.skip_confirmation! #skips email confirmation
+      end # where
+    end # if
   end
 
 end
