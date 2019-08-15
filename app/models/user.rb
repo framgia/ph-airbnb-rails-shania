c9165484b1777c457fb90f847a819153d9bf3637
shania98 <shania.jubelag@sun-asterisk.com>
2019-08-15 13:35:15 +0800

    SNS Authentication with Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+  
+  # Omniauth with Facebook
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.uid = auth.uid
+      user.password = Devise.friendly_token[0,20]
+      user.image = auth.info.image
+      user.skip_confirmation! #skips email confirmation
+    end
+  end
 end
